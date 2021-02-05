5d7b4a9adb6e60d03f97ac85403b7c95e577d964
Laura <G00398298@gmit.ie>
2021-02-05 16:11:52 +0000

    week 03 lab 1

---
+++ week03/extraQuestions.py
@@ -0,0 +1,10 @@
+message = 'I have eaten ' + 99 + ' burritos.'
+print (message)
+# Q.7 - It gives an error message as you can't add an integer to a string without converting the integer first, as follows: 
+message = 'I have eaten ' + str(99) + ' burritos.'
+print (message)
+# Q.8 - eggs is a valid variable name as it starts with a letter, 100 doesn't start with a letter so it is an invalid name.
+# Q.9 - The following functions can be used: 
+# integer: int()
+# floating-point number: float()
+# string: str()
