c7efe1a5f043aa706b4178b3b074fb9341e925d0
Laura <G00398298@gmit.ie>
2021-02-05 16:31:58 +0000

    week 03 lab 2

---
+++ week03/convert.py
@@ -0,0 +1,10 @@
+# convert.py
+# this program converts a user's input of dollars into its' absolute value in cents
+# author: Laura Condon
+
+# asks the user for their input and converts it to a float
+lodgement = float(input("Please enter the amount in dollars that you wish to convert: "))
+# multiples by 100 to convert the input into cents
+cents = lodgement*100
+# returns the absolute value of the user's input as an integer
+print(abs(int(cents)))
