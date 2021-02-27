f5bcb4cce0a4ddcd38fa92c5aa1c3d70d56b050b
Laura <G00398298@gmit.ie>
2021-02-27 16:52:28 +0000

    fixed spacing in lab 04.01.01

--- week04-flow/lab04.01.01.isEven.py
+++ week04-flow/lab04.01.01.isEven.py
@@ -2,9 +2,9 @@
 # and indicates if the number is odd or even
 # Author: Laura Condon
 
-number = int(input("enter an integer: "))
+number = int(input("Enter an integer: "))
 
-if (number % 2) == 0:7
+if (number % 2) == 0:
     print (str(number) + " is an even number")
 else:
-    print(str(number) + " is an odd number")
+    print (str(number) + " is an odd number")
