6dfe5f6185c3983a985da3fdcadaf71f26be8ca8
Laura <G00398298@gmit.ie>
2021-02-27 16:19:51 +0000

    created week04 folder + added lab 1.01

---
+++ week04-flow/lab04.01.01.isEven.py
@@ -0,0 +1,10 @@
+# Program prompts the user for an number 
+# and indicates if the number is odd or even
+# Author: Laura Condon
+
+number = int(input("enter an integer: "))
+
+if (number % 2) == 0:7
+    print (str(number) + " is an even number")
+else:
+    print(str(number) + " is an odd number")
