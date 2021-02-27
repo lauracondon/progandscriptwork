a21b9d2ec497b6227a2f93bfbda737c5ad5b048f
Laura <G00398298@gmit.ie>
2021-02-27 17:07:46 +0000

    adding a while loop to lab04.01.01

--- week04-flow/lab04.01.01.isEven.py
+++ week04-flow/lab04.01.01.isEven.py
@@ -4,6 +4,9 @@
 
 number = int(input("Enter an integer: "))
 
+#while number != -1:
+    #number  = int(input("Wrong! Try entering -1 instead: ")
+
 if (number % 2) == 0:
     print (str(number) + " is an even number")
 else:
